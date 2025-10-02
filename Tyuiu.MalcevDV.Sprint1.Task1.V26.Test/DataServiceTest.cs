81d96a05cdc5b8db32031f04536d54116df2bc19
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-10-02 06:31:34 +0500

    Task1

---
+++ Tyuiu.MalcevDV.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MalcevDV.Sprint1.Task1.V26.Lib;
+namespace Tyuiu.MalcevDV.Sprint1.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.25, res);
+
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint1.Task1.V26/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.MalcevDV.Sprint1.Task1.V26;
+using Tyuiu.MalcevDV.Sprint1.Task1.V26.Lib;
+DataService ds = new DataService();
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #1", width);
+PrintCenteredLine("Тема: Создание решения по таску", width);
+PrintCenteredLine("Задание #1", width);
+PrintCenteredLine("Вариант #26", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Написать программу, которая запрашивает у пользователя исходные данные", width);
+PrintCenteredLine("и вычисляет результат по формуле 5*x/ (2+y) и печатает его на экране", width);
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+Console.WriteLine(new string('*', width));
+Console.WriteLine("Введите значение x:");
+double x, y;
+x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение y:");
+y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine($"{ds.Calculate(x,y)}", width);
+Console.WriteLine(new string('*', width));
