cb142d202cc5140e3bf588f7689c60278e4eab1c
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-10-02 18:40:49 +0500

    Task3

---
+++ Tyuiu.MalcevDV.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MalcevDV.Sprint1.Task3.V11.Lib;
+DataService ds  = new DataService();
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #1", width);
+PrintCenteredLine("Тема: Создание решения по таску", width);
+PrintCenteredLine("Задание #2", width);
+PrintCenteredLine("Вариант #11", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Написать программу, которая запрашивает у пользователя координаты углов", width);
+PrintCenteredLine("и вычисляет площадь заданного ими треугольника с точностью до 3х знаков", width);
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+Console.WriteLine(new string('*', width));
+Console.Write("Введите x1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Введите x3: ");
+double x3 = double.Parse(Console.ReadLine());
+Console.Write("Введите y3: ");
+double y3 = double.Parse(Console.ReadLine());
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine($"{ds.TriangleArea(x1,y1,x2,y2,x3,y3)}", width);
+Console.WriteLine(new string('*', width));
