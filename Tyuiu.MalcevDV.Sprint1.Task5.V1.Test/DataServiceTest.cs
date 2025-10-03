59273ae9eec08d9945c8da22e1207bb87e113230
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-10-04 01:33:21 +0500

    Task5

---
+++ Tyuiu.MalcevDV.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MalcevDV.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.MalcevDV.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x1 = 3.5;
+            double y1 = 2.7;
+            double x2 = 8.1;
+            double y2 = 6.3;
+            var res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MalcevDV.Sprint1.Task5.V1.Lib;
+DataService ds  = new DataService();
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #1", width);
+PrintCenteredLine("Тема: Создание решения по таску", width);
+PrintCenteredLine("Задание #5", width);
+PrintCenteredLine("Вариант #1", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Написать программу, принимает на ввод координаты 2х точек", width);
+PrintCenteredLine("переменных и вычисляет расстояние между ними ", width);
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+Console.WriteLine(new string('*', width));
+Console.WriteLine("Введите координаты первой точки:");
+Console.Write("x1 = ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y1 = ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки:");
+Console.Write("x2 = ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("y2 = ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine($"{ds.DistanceBetweenDots(x1, y1, x2, y2)}", width);
+Console.WriteLine(new string('*', width));
